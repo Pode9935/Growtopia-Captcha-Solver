8c4a8fbf7332bda8c2e9dc6898b385d566d57764
Pode9935 <126120989+Pode9935@users.noreply.github.com>
2023-05-01 12:28:47 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 from io import BytesIO
 from flask import Flask
 from ultralytics import YOLO
@@ -10,35 +10,44 @@ app = Flask(__name__)
 model = YOLO('best.pt')
 
 def RTTEX_PNG(name):
-    with open(r'data/{}.rttex'.format(name), 'rb') as image:
+    with open(r'data\{}.rttex'.format(name), 'rb') as image:
         data = image.read()
 
     width = int.from_bytes(data[12:16], byteorder='little')
     height = int.from_bytes(data[8:12], byteorder='little')
 
-    img = Image.frombytes(mode='RGBA', size=(width, height), data=data[0x7c:])
-    img = img.transpose(method=Image.FLIP_TOP_BOTTOM)
+    imgs = Image.frombytes(mode='RGBA', size=(width, height), data=data[0x7c:])
+    imgs = imgs.transpose(method=Image.FLIP_TOP_BOTTOM)
 
-    buffer = BytesIO()
-    img.save(buffer, format='PNG')
-    png_data = buffer.getvalue()
-
-    with open(r'data/{}.png'.format(name), 'wb') as simg:
-        simg.write(png_data)
+    imgs.save(r'data\{}.png'.format(name), format='PNG')
 
 def yolov8(name):
-    reresults = model.predict(r'data/{}.png'.format(name), conf=0.5)
+    ans = ''
+    img = Image.open(r'data\{}.png'.format(name))
+    reresults = model.predict(img, conf=0.8)
     for result in reresults:
         boxes = result.boxes
         if not 'tensor([],' in str(boxes.xyxy):
             for bbox in boxes:
                 for i, box in enumerate(bbox.xyxy):
                     x, y, w, h = [int(i) for i in box]
-                    return str(x / 512)
+
+                    font = ImageFont.truetype(r'ayar.ttf', 15)
+                    text = "Puzzle: {}".format(round(float(bbox.conf), 4))
+                    img1 = ImageDraw.Draw(img)
+                    img1.rectangle([x, y, w, h], outline = "#ff00ff", width = 2)
+                    img1.rectangle([x, y - 20, x + font.getlength(text), y], fill = "#ff00ff", outline = "#ff00ff")
+                    img1.text((x, y - 20), text, fill = "black", font = font)
+
+                    ans = str(x / 512)
+
+            img.save(r'solved\{}.png'.format(name), 'PNG')
 
         else:
-            shutil.copyfile(r'data/{}.png'.format(name), r'failed/{}.png'.format(name))
-            return 'Failed'
+            shutil.copyfile(r'data\{}.png'.format(name), r'failed\{}.png'.format(name))
+            ans = 'Failed'
+
+    return ans
 
 @app.route('/captcha=<string:pid>')
 def captcha(pid):
@@ -46,7 +55,7 @@ def captcha(pid):
     http = urllib3.PoolManager()
     r = http.request('GET', url)
     if r.status == 200:
-        with open(r'data/{}.rttex'.format(pid), 'wb') as f:
+        with open(r'data\{}.rttex'.format(pid), 'wb') as f:
             f.write(r.data)
 
         RTTEX_PNG(pid)
